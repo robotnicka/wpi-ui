d7c38c317e5865de08d5f106b45c3d332c12ef3e
Joe Terranova <joeterranova@gmail.com>
2018-04-25 04:04:54 +0000

    Working on member search!

--- src/app/modules/core/models/index.ts
+++ src/app/modules/core/models/index.ts
@@ -1,4 +1,5 @@
 export { Office } from './office';
 export { User } from './user';
+export { MemberSearch } from './member-search';
 export { OrgUnit } from './org-unit';
 export { OrgUnitSearch } from './org-unit-search';
---
+++ src/app/modules/core/models/member-search.ts
@@ -0,0 +1,8 @@
+export class MemberSearch {
+  name: string;
+  email: string;
+  membershipNumber: string;
+  type: string;
+  limit: number;
+  offset: number;
+}
