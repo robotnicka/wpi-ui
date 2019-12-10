d09e8e3a843560699ed6078165bd2a8cbf9a37d9
Joe Terranova <joe.terranova@toomuchmedia.com>
2019-12-10 18:32:04 -0500

    More updating

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { GuardService } from 'app/modules/core/guard.service';
 const routes: Routes = [
-	{ path: '', loadChildren: 'app/modules/home/home.module#HomeModule' },
-	{ path: 'auth', loadChildren: 'app/modules/auth/auth.module#AuthModule'},
-	{ path: 'locations', loadChildren: 'app/modules/locations/locations.module#LocationsModule', canActivate: [GuardService]},
-	{ path: 'members', loadChildren: 'app/modules/members/members.module#MembersModule', canActivate: [GuardService]}
+	{ path: '', loadChildren: () => import('app/modules/home/home.module').then(m => m.HomeModule) },
+	{ path: 'auth', loadChildren: () => import('app/modules/auth/auth.module').then(m => m.AuthModule)},
+	{ path: 'locations', loadChildren: () => import('app/modules/locations/locations.module').then(m => m.LocationsModule), canActivate: [GuardService]},
+	{ path: 'members', loadChildren: () => import('app/modules/members/members.module').then(m => m.MembersModule), canActivate: [GuardService]}
 	
 ];
 
